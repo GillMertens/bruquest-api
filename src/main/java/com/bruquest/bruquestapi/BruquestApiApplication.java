ab89a8d2c22b9930c2001a8569d6af3be5d735d9
Gill Mertens <gill.mertens@student.ehb.be>
2023-11-26 21:34:49 +0100

    initial commit

---
+++ src/main/java/com/bruquest/bruquestapi/BruquestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bruquest.bruquestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BruquestApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BruquestApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bruquest/bruquestapi/BruquestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruquest.bruquestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BruquestApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
